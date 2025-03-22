b2238336c2b454f5aee36778c95f1ec8b01be463
hmabubakar313 <refocuse313@gmail.com>
2025-03-23 04:36:26 +0500

    set up buyers page

--- frontend/farm_fe/src/app/buyers/page.js
+++ frontend/farm_fe/src/app/buyers/page.js
@@ -8,6 +8,7 @@ import {
   FaPhoneAlt,
 } from "react-icons/fa";
 import { PiShoppingCartSimple } from "react-icons/pi";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { CiSearch } from "react-icons/ci";
 import Image from "next/image";
 import { FaPhone } from "react-icons/fa6";
@@ -69,12 +70,418 @@ const Header = () => {
           </button>
         </div>
       </nav>
-      <div className="relative w-full px-14 py-10">
-  <div className="relative ml-20">
-    <Image src="/buyers_bg.jpg" width={850} height={100} />
-    <p className="absolute top-2 left-8 text-black text-lg">
-      Aggriconnect Hub
-    </p>
+      <div className="flex justify-center items-start py-10 space-x-4 w-full">
+        <div
+          className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+          style={{ width: "850px" }}
+        >
+          <Image
+            src="/buyers_bg.jpg"
+            width={850}
+            height={100}
+            className="rounded-lg"
+            alt=""
+          />
+          <h2 className="absolute text-black font-bold top-1/4 left-8 -translate-y-2 text-xl">
+            Aggriconnect Hub
+          </h2>
+          <div className="w-1 h-14 bg-gray-400 absolute left-6 top-1/3"></div>
+          <h4 className="absolute top-1/3 left-8 -translate-y-1 text-lg">
+            Sale up to{" "}
+            <span className="text-red-500 font-semibold">30% OFF</span>
+          </h4>
+          <p className="absolute top-1/3 left-8 mt-4 text-lg">
+            Free shipping on all your orders
+          </p>
+          <button className="absolute top-1/3 left-8 mt-16 px-6 py-3 text-green-500 bg-white border border-green-500 rounded-full">
+            Shop Now ➝
+          </button>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          <div
+            className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+            style={{ width: "450px" }}
+          >
+            <Image
+              src="/buyers_right.jpg"
+              width={450}
+              height={100}
+              className="rounded-lg"
+              alt=""
+            />
+            <h4 className="absolute left-6 top-6 text-lg">Summer Sale</h4>
+            <h2 className="absolute top-14 left-6 text-lg">75% OFF</h2>
+            <p className="absolute top-20 left-6 text-lg">
+              Only vegetables and fruit
+            </p>
+            <button className="absolute top-28 left-6 text-lg text-green-800">
+              Shop Now ➝
+            </button>
+          </div>
+
+          <div
+            className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+            style={{ width: "450px", height: "250px" }}
+          >
+            <Image
+              src="/buyers_right.jpg"
+              width={450}
+              height={100}
+              className="rounded-lg"
+              alt=""
+            />
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+              <p className="text-white">Best Deal</p>
+              <h1 className="text-2xl text-white">
+                Special Products <br></br> Deal of Month{" "}
+              </h1>
+              <p className="text-base text-white ">Fresh seasonal offers</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto mt-8">
+        <div className="flex gap-8 bg-white border border-gray-300 rounded-xl overflow-hidden items-center py-4 px-4 justify-center ml-4">
+          <div className="flex flex-col gap-2 py-2">
+            <p className="text-xl font-bold">Free Shipping</p>
+            <p className="text-gray-500">Free shipping on all orders</p>
+          </div>
+
+          <div className="flex flex-col gap-2 py-2">
+            <p className="text-xl font-bold">Customer Support 24/7</p>
+            <p className="text-gray-500">Instant access to Support</p>
+          </div>
+
+          <div className="flex flex-col gap-2 py-2">
+            <p className="text-xl font-bold">100% Secure Payment</p>
+            <p className="text-gray-500">We ensure your money is safe</p>
+          </div>
+
+          <div className="flex flex-col gap-2 py-2">
+            <p className="text-xl font-bold">Money-Back Guarantee</p>
+            <p className="text-gray-500">30 Days Money-Back Guarantee</p>
+          </div>
+        </div>
+      </div>
+      <h1 className="flex justify-start px-14 py-8">Popular Categories</h1>
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-6 gap-2 bg-white rounded-xl overflow-hidden ">
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto py-4">
+        <div className="grid grid-cols-6 gap-2 bg-white rounded-xl overflow-hidden ">
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+
+          <div className="flex flex-col items-center p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition duration-300">
+            <Image
+              className="rounded-md object-cover"
+              src={"/ctg_1.jpg"}
+              height={120}
+              width={120}
+              alt=""
+            />
+            <p className="text-lg font-semibold mt-3">Free Shipping</p>
+          </div>
+        </div>
+      </div>
+      <h1 className="flex justify-start px-14 py-8">Popular Products</h1>
+
+      <div className="max-w-7xl mx-auto  overflow-x-auto">
+        <div className="flex flex-nowrap gap-0">
+          <div className="w-1/4 bg-white border  shadow-md p-4 relative">
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-sm font-semibold px-3 py-1 rounded-lg">
+              Sale 50%
+            </span>
+            <Image
+              className="w-full h-48 object-cover "
+              src="/ctg_3.jpg"
+              height={450}
+              width={450}
+              alt=""
+            />
+            <div className="mt-4">
+              <h3 className="text-gray-700 text-lg font-medium">Green Apple</h3>
+              <p className="text-black text-xl font-bold">100</p>
+              <div className="flex items-center mt-1">
+                <span className="text-yellow-500 text-lg">★★★★☆</span>
+              </div>
+            </div>
+            <button className="absolute bottom-4 right-4 bg-gray-100 p-3 rounded-full shadow-md">
+              <HiOutlineShoppingBag />
+            </button>
+          </div>
+
+          <div className="w-1/4 bg-white border  shadow-md p-4 relative">
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-sm font-semibold px-3 py-1 rounded-lg">
+              Sale 50%
+            </span>
+            <Image
+              className="w-full h-48 object-cover rounded-lg"
+              src="/ctg_3.jpg"
+              height={450}
+              width={450}
+              alt=""
+            />
+            <div className="mt-4">
+              <h3 className="text-gray-700 text-lg font-medium">Green Apple</h3>
+              <p className="text-black text-xl font-bold">100</p>
+              <div className="flex items-center mt-1">
+                <span className="text-yellow-500 text-lg">★★★★☆</span>
+              </div>
+            </div>
+            <button className="absolute bottom-4 right-4 bg-gray-100 p-3 rounded-full shadow-md">
+              <HiOutlineShoppingBag />
+            </button>
+          </div>
+
+          <div className="w-1/4 bg-white border  shadow-md p-4 relative">
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-sm font-semibold px-3 py-1 rounded-lg">
+              Sale 50%
+            </span>
+            <Image
+              className="w-full h-48 object-cover rounded-lg"
+              src="/ctg_3.jpg"
+              height={450}
+              width={450}
+              alt=""
+            />
+            <div className="mt-4">
+              <h3 className="text-gray-700 text-lg font-medium">Green Apple</h3>
+              <p className="text-black text-xl font-bold">100</p>
+              <div className="flex items-center mt-1">
+                <span className="text-yellow-500 text-lg">★★★★☆</span>
+              </div>
+            </div>
+            <button className="absolute bottom-4 right-4 bg-gray-100 p-3 rounded-full shadow-md">
+              <HiOutlineShoppingBag />
+            </button>
+          </div>
+          <div className="w-1/4 bg-white border  shadow-md p-4 relative">
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-sm font-semibold px-3 py-1 rounded-lg">
+              Sale 50%
+            </span>
+            <Image
+              className="w-full h-48 object-cover rounded-lg"
+              src="/ctg_3.jpg"
+              height={450}
+              width={450}
+              alt=""
+            />
+            <div className="mt-4">
+              <h3 className="text-gray-700 text-lg font-medium">Green Apple</h3>
+              <p className="text-black text-xl font-bold">100</p>
+              <div className="flex items-center mt-1">
+                <span className="text-yellow-500 text-lg">★★★★☆</span>
+              </div>
+            </div>
+            <button className="absolute bottom-4 right-4 bg-gray-100 p-3 rounded-full shadow-md">
+              <HiOutlineShoppingBag />
+            </button>
+          </div>
+          <div className="w-1/4 bg-white border  shadow-md p-4 relative">
+            <span className="absolute top-3 left-3 bg-red-500 text-white text-sm font-semibold px-3 py-1 rounded-lg">
+              Sale 50%
+            </span>
+            <Image
+              className="w-full h-48 object-cover rounded-lg"
+              src="/ctg_3.jpg"
+              height={450}
+              width={450}
+              alt=""
+            />
+            <div className="mt-4">
+              <h3 className="text-gray-700 text-lg font-medium">Green Apple</h3>
+              <p className="text-black text-xl font-bold">100</p>
+              <div className="flex items-center mt-1">
+                <span className="text-yellow-500 text-lg">★★★★☆</span>
+              </div>
+            </div>
+            <button className="absolute bottom-4 right-4 bg-gray-100 p-3 rounded-full shadow-md">
+              <HiOutlineShoppingBag />
+            </button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="flex gap-4 px-6 py-16">
+  <div
+    className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+    style={{ width: "424px", height: "536px" }}
+  >
+    <Image
+      src="/buyers_right.jpg"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-lg"
+      alt=""
+    />
+    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+      <p className="text-white">Best Deal</p>
+      <h1 className="text-2xl text-white">
+        Special Products <br /> Deal of Month
+      </h1>
+      <p className="text-base text-white">Fresh seasonal offers</p>
+    </div>
+  </div>
+
+  <div
+    className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+    style={{ width: "424px", height: "536px" }}
+  >
+    <Image
+      src="/buyers_right.jpg"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-lg"
+      alt=""
+    />
+    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+      <p className="text-white">Best Deal</p>
+      <h1 className="text-2xl text-white">
+        Special Products <br /> Deal of Month
+      </h1>
+      <p className="text-base text-white">Fresh seasonal offers</p>
+    </div>
+  </div>
+
+  <div
+    className="relative bg-white shadow-lg rounded-lg overflow-hidden"
+    style={{ width: "424px", height: "536px" }}
+  >
+    <Image
+      src="/buyers_right.jpg"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-lg"
+      alt=""
+    />
+    <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+      <p className="text-white">Best Deal</p>
+      <h1 className="text-2xl text-white">
+        Special Products <br /> Deal of Month
+      </h1>
+      <p className="text-base text-white">Fresh seasonal offers</p>
+    </div>
   </div>
 </div>
 
